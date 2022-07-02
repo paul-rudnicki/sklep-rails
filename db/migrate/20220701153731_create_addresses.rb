4863842a42b37563b7c2694332debd6ae31196ab
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-07-02 17:39:08 +0200

    change db

--- db/migrate/20220701153731_create_addresses.rb
+++ db/migrate/20220701153731_create_addresses.rb
@@ -1,13 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
-    create_table :addresses do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :city
-      t.string :zip
-      t.string :street
-      t.string :email
-      t.belongs_to :order, null: false, foreign_key: true
+    create_table :orders do |t|
+      t.text :comment
+      t.belongs_to :shipping_type, null: false, foreign_key: true
+      t.decimal :shipping_cost
 
       t.timestamps
     end
--- db/migrate/20220701153927_create_orders.rb
+++ db/migrate/20220701153927_create_orders.rb
@@ -1,11 +1,16 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
-    create_table :orders do |t|
-      t.text :comment
-      t.belongs_to :shipping_type, null: false, foreign_key: true
-      t.decimal :shipping_cost
+    create_table :addresses do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :city
+      t.string :zip
+      t.string :street
+      t.string :email
+      t.belongs_to :order, null: false, foreign_key: true
 
       t.timestamps
+
     end
   end
 end
